19b769549ad1fb05b8964d8b4a94a312ea30725b
Hayashi Haruto <c0a23121ff@edu.teu.ac.jp>
2024-11-19 16:13:56 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -12,13 +12,16 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像の読み込み
     koukaton_img = pg.image.load("fig/3.png") #こうかとん画像の読み込み
     koukaton_img = pg.transform.flip(koukaton_img, True, False) #こうかとん画像を左右反転
+    bgfl_img = pg.transform.flip(bg_img, True, False) #背景画像を左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [-tmr%-800, 0]) #screen surface に背景画像を描画
-        
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0]) #screen surface に背景画像を描画
+        screen.blit(bgfl_img, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bgfl_img, [-x+4800, 0])
 
         screen.blit(koukaton_img, [300, 200]) #screen surface にこうかとん画像を描画
 

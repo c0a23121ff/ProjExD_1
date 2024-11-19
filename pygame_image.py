6f32c19314a3a9f432e29c566626c9e1f4b00401
Hayashi Haruto <c0a23121ff@edu.teu.ac.jp>
2024-11-19 15:56:30 +0900

    練習６

--- pygame_image.py
+++ pygame_image.py
@@ -6,24 +6,25 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
-    pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
-    koukaton_img = pg.image.load("fig/3.png")
-    koukaton_img = pg.transform.flip(koukaton_img, True, False)
+    pg.display.set_caption("はばたけ！こうかとん") #ウィンドウのタイトルを設定
+    screen = pg.display.set_mode((800, 600)) #スクリーンの解像度を設定
+    clock  = pg.time.Clock() #フレームレート調整用の Clock オブジェクトの生成
+    bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像の読み込み
+    koukaton_img = pg.image.load("fig/3.png") #こうかとん画像の読み込み
+    koukaton_img = pg.transform.flip(koukaton_img, True, False) #こうかとん画像を左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #screen surface に背景画像を描画
+        screen.blit(bg_img, [-tmr%-800, 0]) #screen surface に背景画像を描画
+        
 
         screen.blit(koukaton_img, [300, 200]) #screen surface にこうかとん画像を描画
 
-        pg.display.update()
+        pg.display.update() #画面を更新
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200) #フレームレートを 200fps に設定
 
 
 if __name__ == "__main__":
